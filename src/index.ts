85ea190888b8ec67754c39e231eb0085576edc0e
biud436 <biud436@gmail.com>
2022-01-04 13:35:56 +0900

    리눅스 시스템이 아닐 때 오류 출력

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 import inquirer from "inquirer";
+import { isForOfStatement } from "typescript";
 
+/**
+ * 기본 서비스 파일의 경로입니다.
+ */
 const TARGET_SERVICE_FILE_PATH = `/etc/systemd/system/`;
 
 /**
@@ -16,7 +20,12 @@ const TARGET_SERVICE_FILE_PATH = `/etc/systemd/system/`;
 const manager = (() => {
   return new (class {
     isRoot() {
-      const uid = process.geteuid();
+      const platform = process.platform;
+      console.log(platform);
+      if (platform !== "linux") {
+        return false;
+      }
+      const uid = process.getuid();
       const isRoot = uid === 0;
 
       return isRoot;
@@ -24,6 +33,46 @@ const manager = (() => {
   })();
 })();
 
+/**
+ * 앱을 생성합니다.
+ */
+const app = new App();
+
+/**
+ * 서비스 파일을 생성합니다.
+ *
+ * @param data
+ */
+function createServiceFile(data: any) {
+  /**
+   * @type {String}
+   */
+  let filename = argv.f || argv.file;
+
+  if (!manager.isRoot()) {
+    let defaultErrorMessage = "root 권한이 없습니다";
+    if (process.platform !== "linux") {
+      defaultErrorMessage =
+        defaultErrorMessage + ". 그리고 리눅스 시스템이 아닙니다.";
+    }
+    throw new Error(defaultErrorMessage);
+  } else {
+    // 타겟을 설정합니다.
+    let target = TARGET_SERVICE_FILE_PATH;
+    if (!filename.startsWith(target)) {
+      filename = path.join(target, filename);
+    }
+    // 서비스 파일을 생성합니다.
+    fs.writeFileSync(filename, data, "utf-8");
+
+    // 서비스를 재시작합니다.
+    app.emit("build");
+  }
+}
+
+/**
+ * 서비스를 인터렉티브하게 생성합니다.
+ */
 if (argv.q) {
   inquirer
     .prompt([
@@ -44,58 +93,45 @@ if (argv.q) {
       },
       {
         type: "input",
-        name: "file",
-        message: "서비스 파일의 이름을 입력하세요 (예: badge-server)",
+        name: "cmd",
+        message: "명령어를 입력하세요",
       },
     ])
     .then((answers) => {
-      const { desc, requires, identifier, file } = answers;
+      const { desc, requires, identifier, cmd } = answers;
+
+      app.on("ready", () => {
+        // 서비스 객체를 생성합니다.
+        const service = new Service(
+          {
+            desc: desc || "",
+            requires: requires || "",
+            cmd: cmd || "",
+            identifier: identifier || "",
+          },
+          createServiceFile
+        );
+      });
     })
     .catch((err) => {
       console.warn(err);
     });
-}
-
-function createServiceFile(data: any) {
-  /**
-   * @type {String}
-   */
-  let filename = argv.f || argv.file;
-
-  const info = os.userInfo();
-  const uid = process.geteuid();
-
-  if (!manager.isRoot()) {
-    throw new Error("root 권한이 없습니다");
-  } else {
-    // 타겟을 설정합니다.
-    let target = TARGET_SERVICE_FILE_PATH;
-    if (!filename.startsWith(target)) {
-      filename = path.join(target, filename);
-    }
-    // 서비스 파일을 생성합니다.
-    fs.writeFileSync(filename, data, "utf-8");
-
-    // 서비스를 재시작합니다.
-    app.emit("build");
-  }
+} else {
+  app.on("ready", () => {
+    // 서비스 객체를 생성합니다.
+    const service = new Service(
+      {
+        desc: argv.desc || "",
+        requires: argv.r || argv.requires || "",
+        cmd: argv.cmd || "",
+        identifier: argv.identifier || "",
+      },
+      createServiceFile
+    );
+  });
 }
 
 /**
- * 앱 관리자를 생성합니다.
+ * 이벤트를 실행합니다.
  */
-const app = new App();
-app.on("ready", () => {
-  // 서비스 객체를 생성합니다.
-  const service = new Service(
-    {
-      desc: argv.desc || "",
-      requires: argv.r || argv.requires || "",
-      cmd: argv.cmd || "",
-      identifier: argv.identifier || "",
-    },
-    createServiceFile
-  );
-});
-
 app.emit("ready");
